06f03c113cc5a290c5adb2a81a519b73c5cebeb0
polartemis <kat333859@gmail.com>
2023-07-03 15:33:39 +0300

    initial

---
+++ 1506.py
@@ -0,0 +1,25 @@
+'''def strcounter(s): # O (N*M) = 36
+    for sym in set(s): # set - функция приведения к множеству
+        count = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                count+=1
+        print(sym,count)
+strcounter('abcnra')
+
+def strcounter(s): # O (N^2) = 36
+    for sym in s:
+        count = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                count+=1
+        print(sym,count)
+strcounter('abcnra')'''
+
+def strcounter(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+    for sym, count in syms_counter.items():
+        print(sym, count)
+strcounter('whstgdbdsf')
