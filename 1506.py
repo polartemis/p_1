2407d31e6e6dcfbe651d6d76ea54feb5be478644
polartemis <kat333859@gmail.com>
2023-07-03 15:36:41 +0300

    2 комит

--- 1506.py
+++ 1506.py
@@ -20,6 +20,5 @@ def strcounter(s):
     syms_counter = {}
     for sym in s:
         syms_counter[sym] = syms_counter.get(sym, 0) + 1
-    for sym, count in syms_counter.items():
-        print(sym, count)
-strcounter('whstgdbdsf')
+
+print ('изменения')
