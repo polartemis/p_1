735f5714ae5844781ae3097d43c3624351008450
polartemis <kat333859@gmail.com>
2023-08-24 16:20:44 +0300

    домашка 5 занятие

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Успешно!')
+    return render(request, 'index.html')
 
-# Create your views here.
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
